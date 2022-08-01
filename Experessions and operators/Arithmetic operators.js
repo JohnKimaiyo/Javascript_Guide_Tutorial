cf768966bf486453e10d46211d8469936d69e1ce
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-01 16:22:49 +0300

    decremet operator

--- Experessions and operators/Arithmetic operators.js
+++ Experessions and operators/Arithmetic operators.js
@@ -1,10 +1,9 @@
 // remainder (%) example 1
-console.log(13%5);
+console.log(13 % 5);
 
-console.log(-13%5);
-console.log(4%2);
-console.log(-4%2);
-
+console.log(-13 % 5);
+console.log(4 % 2);
+console.log(-4 % 2);
 
 // increment (++) example 2s
 
@@ -17,3 +16,13 @@ const b = ++a;
 
 console.log(`a:${a} , b;${b}`);
 
+// decrement (--) exmaple 3
+
+let m = 3;
+const n = m--;
+
+console.log(`x:${m}, y:${n}`);
+
+let o = 3;
+const p = --a;
+console.log = `a:${a}, b:${b}`;
